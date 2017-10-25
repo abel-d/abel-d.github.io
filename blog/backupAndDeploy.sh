d065a132a611814b19ac2a67fed55036f8fb3c73
abel-d <1009224896@qq.com>
2017-10-26 00:14:23 +0800

    backup blog

--- blog/backupAndDeploy.sh
+++ blog/backupAndDeploy.sh
@@ -1,4 +1,4 @@
-!#/user/bin/bash
+#!/user/bin/bash
 hexo clean
 cp -R /Users/abel_d/GitProject/gitHubBlog/blog /Users/abel_d/GitProject/gitHubBlog/abel-d.github.io
 hexo g
@@ -6,3 +6,4 @@ hexo d
 cd /Users/abel_d/GitProject/gitHubBlog/abel-d.github.io
 git add .
 git commit -m "backup blog"
+git push
